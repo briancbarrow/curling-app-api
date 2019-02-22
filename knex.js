a65deb5c84465c24e39f63648753227cdf8aede2
Alexander <alexpuhldeveloper@gmail.com>
2019-02-21 19:17:37 -0700

    updated gitignore

--- knex.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const knexConfig = require('./knexfile');
-
-const environment = process.env.NODE_ENV || 'development';
-
-module.exports = require('knex')(knexConfig[environment]);
